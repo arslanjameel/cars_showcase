c1e38589fe00ce3eb01158ce101b1f81135c6bbd
arslanjameel <arslanjameel40@gmail.com>
2024-03-04 16:46:07 +0500

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,50 @@
+import {CarCard, CustomFilter, Hero, SearchBar, ShowMore} from "@/components";
+import {fetchCars} from "@/utlis";
+import {fuels, yearsOfProduction} from "@/constants";
+
+export default async function Home({searchParams}:any) {
+    const allCars = await fetchCars({
+        manufacturer: searchParams.manufacturer || "",
+        year: searchParams.year || 2015,
+        fuel: searchParams.fuel || "",
+        limit: searchParams.limit || 10,
+        model: searchParams.model || ""
+    });
+    const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
+    return (
+        <main className="overflow-hidden">
+            <Hero/>
+            <div className="mt-12 padding-x padding-y max-width" id="discover">
+                <div className="home__text-container">
+                    <h1 className="text-4xl font-extrabold"> Car Catalogue</h1>
+                    <p>Explore the cars you might like</p>
+                </div>
+                <div className="home__filters">
+                    <SearchBar/>
+                    <div className="home__filter-container">
+                        <CustomFilter title="fuel" options={fuels}/>
+                        <CustomFilter title="year" options={yearsOfProduction}/>
+                    </div>
+                </div>
+                {!isDataEmpty ?
+                    (<section>
+                        <div className="home__cars-wrapper">
+                            {allCars?.map((car,index) => (
+                                <CarCard car={car} key={index+car.make+car.model+car.year}/>
+                            ))}
+                        </div>
+                        <ShowMore pageNumber={(searchParams.limit || 10)/10} isNext={(searchParams.limit||10)>allCars.length} />
+                    </section>)
+                    : (
+                        <div className="home__error-container">
+                            <h2 className="text-black text-xl font-bold">
+                                Oops, no results
+                            </h2>
+                            <p>{allCars?.message}</p>
+                        </div>
+                    )}
+            </div>
+        </main>
+    );
+}
